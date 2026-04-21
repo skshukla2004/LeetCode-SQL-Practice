71b2e5cde7999a94c1a614babb6ab92375b029c7
Shashikant Shukla <sks7523830807@gmail.com>
2026-04-21 16:06:07 +0530

    Implement SQL query for person and address details
    Added SQL query to select person details with address information.

--- 2026-04-21_SQL_001.sql
+++ 2026-04-21_SQL_001.sql
@@ -1,4 +1,8 @@
+/* Write a solution to report the first name, last name, city, and state of each person in the Person table. 
+If the address of a personId is not present in the Address table, report null instead.
+Return the result table in any order.*/
 /* Write your T-SQL query statement below */
+
 select p.firstname, p.lastname, a.city, a.state from person as p 
  left join address as a 
  on p.personid = a.personid
