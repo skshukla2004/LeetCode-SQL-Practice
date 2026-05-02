7a478ee8c0ae15904149767aa32e8c3137fe943c
Shashikant Shukla <sks7523830807@gmail.com>
2026-05-02 23:08:03 +0530

    Create 2026-05-02_SQL_012.sql

---
+++ 2026-05-02_SQL_012.sql
@@ -0,0 +1,9 @@
+/*Write a solution to find all the classes that have at least five students.
+Return the result table in any order.*/
+
+# Code
+/* Write your T-SQL query statement below */
+Select class
+from Courses
+group  by class
+having Count(student) >= 5;
