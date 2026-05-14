9101746238f61c581ff911e6cad4db1b4fa2d2c7
Shashikant Shukla <sks7523830807@gmail.com>
2026-05-14 23:41:14 +0530

    .
    Add SQL query to retrieve products with at least 100 units ordered in February 2020.

---
+++ 2026-05-14_SQL_024.sql
@@ -0,0 +1,13 @@
+/* Question : Write a solution to get the names of products that have at least 100 units ordered in February 2020 and their amount.
+Return the result table in any order.*/
+# Code 
+/* Write your T-SQL query statement below */
+SELECT p.product_name,
+       SUM(o.unit) AS unit
+FROM Products p
+JOIN Orders o
+ON p.product_id = o.product_id
+WHERE o.order_date >= '2020-02-01'
+AND o.order_date < '2020-03-01'
+GROUP BY p.product_id, p.product_name
+HAVING SUM(o.unit) >= 100;
