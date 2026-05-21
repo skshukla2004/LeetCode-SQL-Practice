890bca2a5b787223f6e541d7836133f9b35a2185
Shashikant Shukla <sks7523830807@gmail.com>
2026-05-21 13:14:04 +0530

    ,
    Updated SQL query comment to specify the task of finding product IDs that are both low fat and recyclable.

--- 2026-05-21_SQL_031.sql
+++ 2026-05-21_SQL_031.sql
@@ -1,3 +1,6 @@
-/* Question :   Write your T-SQL query statement below */
+/* Question :Write a solution to find the ids of products that are both low fat and recyclable.
+   Return the result table in any order.*/
+#  Code  
+/*  Write your T-SQL query statement below */
   select product_id from products
   where low_fats='y' and recyclable='y'
