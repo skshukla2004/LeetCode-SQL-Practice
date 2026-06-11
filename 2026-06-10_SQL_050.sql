8e862fad0c5cb6bfe89e89db5a09861fd62075f2
Shashikant Shukla <sks7523830807@gmail.com>
2026-06-11 23:23:15 +0530

    Update 2026-06-10_SQL_050.sql

--- 2026-06-10_SQL_050.sql
+++ 2026-06-10_SQL_050.sql
@@ -1,1 +1,11 @@
-/* */
+/* Question No' 178: Write a solution to find the rank of the scores. The ranking should be calculated according to the following rules:
+The scores should be ranked from the highest to the lowest.
+If there is a tie between two scores, both should have the same ranking.
+After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+Return the result table ordered by score in descending order.*/
+# Code 
+Select
+    score,
+    DENSE_RANK() OVER (ORDER BY score DESC) AS rank
+From Scores
+Order by score DESC;
